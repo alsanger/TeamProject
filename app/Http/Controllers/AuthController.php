9d1257d1f817de37d84fdf4d1365d1ca1551ab35
Puf <puffoxx@gmail.com>
2024-08-06 20:35:58 +0300

    form sign in

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function showSignIn()
+    {
+        return view('auth.signin', ['hideHeader' => true]);
+    }
+
+    public function showSignUp()
+    {
+        return view('auth.signup', ['hideHeader' => true]);
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Doctrine\DBAL\Types\Type;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!Type::hasType('timestamp')) {
+            Type::addType('timestamp', 'Doctrine\DBAL\Types\DateTimeType');
+        }
     }
 }
